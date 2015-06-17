2c7454b66cc9f330d251cbe511db36f005e05502
Matjaz Gregoric <mtyaka@gmail.com>
2015-06-17 09:08:04 +0200

    Modify run-automated-task.sh script.
    This makes some changes specific to the Rue89 installation.

--- automation/run-automated-task.sh
+++ automation/run-automated-task.sh
@@ -14,21 +14,21 @@ TASKS_BIN=$VENV_ROOT/analytics-tasks/bin
 CONF_BIN=$VENV_ROOT/analytics-configuration/bin
 
 . $CONF_BIN/activate
-make -C analytics-configuration provision.emr
+make -C /home/jenkins/edx-analytics-configuration provision.emr
 
 function terminate_cluster() {
     . $CONF_BIN/activate
-    make -C analytics-configuration terminate.emr
+    make -C /home/jenkins/edx-analytics-configuration terminate.emr
 }
 if [ "$TERMINATE" = "true" ]; then
     trap terminate_cluster EXIT
 fi
 
 . $TASKS_BIN/activate
-make -C analytics-tasks install
+make -C /edx/app/edx-analytics-pipeline/edx-analytics-pipeline install
 
 # Define task on the command line, including the task name and all of its arguments.
 # All arguments provided on the command line are passed through to the remote-task call.
-remote-task --job-flow-name="$CLUSTER_NAME" --branch $TASKS_BRANCH --wait --log-path $WORKSPACE/logs/ --remote-name automation --user $TASK_USER --secure-config-branch $SECURE_BRANCH --secure-config-repo $SECURE_REPO --secure-config $SECURE_CONFIG "$@"
+remote-task --job-flow-name="$CLUSTER_NAME" --repo "https://github.com/rue89-tech/edx-analytics-pipeline.git" --wait --log-path $WORKSPACE/logs/ --remote-name automation --user hadoop --sudo-user hadoop --override-config "$OVERRIDE_CONFIG" "$@"
 
 cat $WORKSPACE/logs/* || true

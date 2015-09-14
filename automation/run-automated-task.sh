deb25801c5ac6bd6a7c0df09b5dcbda45b217816
Matjaz Gregoric <mtyaka@gmail.com>
2015-09-14 09:49:54 +0200

    Set pipeline branch.

--- automation/run-automated-task.sh
+++ automation/run-automated-task.sh
@@ -29,6 +29,6 @@ make -C /edx/app/edx-analytics-pipeline/edx-analytics-pipeline install
 
 # Define task on the command line, including the task name and all of its arguments.
 # All arguments provided on the command line are passed through to the remote-task call.
-remote-task --job-flow-name="$CLUSTER_NAME" --repo "https://github.com/rue89-tech/edx-analytics-pipeline.git" --wait --log-path $WORKSPACE/logs/ --remote-name automation --user hadoop --sudo-user hadoop --override-config "$OVERRIDE_CONFIG" "$@"
+remote-task --job-flow-name="$CLUSTER_NAME" --repo "https://github.com/rue89-tech/edx-analytics-pipeline.git" --branch "rue89-release" --wait --log-path $WORKSPACE/logs/ --remote-name automation --user hadoop --sudo-user hadoop --override-config "$OVERRIDE_CONFIG" "$@"
 
 cat $WORKSPACE/logs/* || true
